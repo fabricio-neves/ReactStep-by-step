feba832728659e6a4761f613be56d9f2bc65137e
FabsSeven <75510266+fabricio-neves@users.noreply.github.com>
2021-01-02 01:50:19 +0000

    Hello World

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import React from "react";
-import ReactDom from "react-dom";
+var React = require("react");
+var ReactDom = require("react-dom");
 
-ReactDom.render(
-  <div>
-    <h1>My Favorite Food</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
-  </div>,
-  document.getElementById("root")
-);
+// ReactDom.render(WHAT TO SHOW, WHERE TO SHOW)
+ReactDom.render(<h1>Hello World</h1>, document.getElementById("root"));
+//WHAT TO SHOW IS COMPILED BY BABEL FOR ANY BROWSER TO BE ABLE TO RENDER ALMOST ANYTHING!
+//https://babeljs.io/
+
+//var h1 = document.createElement("h1");
+//h1.innerHTML = "Hello World!";
+//document.getElementById("root").appendChild(h1);
